96820b6bc3c39699c8d4e25309c44b027f1bc901
Benjamin Hall <hallb1016@gmail.com>
2021-05-16 16:12:14 -0400

    Initial commit

---
+++ BotBase/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using BotBase.Databases;
+using System.Threading.Tasks;
+
+namespace BotBase
+{
+    public static class DatabaseManager
+    {
+        public static readonly MainDatabase database = new();
+
+        public static async Task InitAsync() =>
+            await Task.WhenAll(
+                database.InitAsync()
+            );
+
+        public static async Task CloseAsync() =>
+            await Task.WhenAll(
+                database.CloseAsync()
+            );
+    }
+}
